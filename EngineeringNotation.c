7499d10d3f564d549aeadea3a3f740b0cabfaeb5
Luis M. Lopez <luismlopez2004@gmail.com>
2025-04-15 21:31:03 -0400

    Update EngineeringNotation.c

--- EngineeringNotation.c
+++ EngineeringNotation.c
@@ -74,7 +74,6 @@ void engineeringNotationConverter(double userNumber){
             exponentCount++; //Increase exponent count
         }
     }
-
     else{
         while(engNotationNumber < 1){ //In the lower outside range
             engNotationNumber *= 10; //Multiplies it to bring it in the range of 1 - 999
